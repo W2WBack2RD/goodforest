3f8b5cea59062d991e139f0768514ce8fe50a773
Shoval Ichia <shovi743@gmail.com>
2022-01-18 22:35:34 +0200

    add button Shoval

---
+++ client/api/example.js
@@ -0,0 +1,5 @@
+import request from "superagent";
+import { handleSuccess, handleError } from "_utils/api";
+
+export const getExample = () =>
+  request.get("/api/example").then(handleSuccess).catch(handleError);
