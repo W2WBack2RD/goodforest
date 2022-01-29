caf51912c24bfb1e43afd8a8526308c5d9036658
Shoval Ichia <shovi743@gmail.com>
2022-01-29 20:12:51 +0200

    after changes - #1

--- client/components/pages/WelcomePage/WelcomePage.js
+++ client/components/pages/WelcomePage/WelcomePage.js
@@ -1,34 +1,8 @@
-import React, { useEffect } from "react";
-// import { useDispatch, useSelector } from "react-redux";
-// import { push } from "connected-react-router";
-// import R from "ramda";
-// import request from "superagent";
-// import Section from "react-bulma-companion/lib/Section";
-// import Container from "react-bulma-companion/lib/Container";
+import React from "react";
 import Title from "react-bulma-companion/lib/Title";
 import Button from "react-bulma-companion/lib/Button";
 
 export default function WelcomePage() {
-  // const dispatch = useDispatch();
-  // const { user } = useSelector(R.pick(["user"]));
-  // const [response, setResponse] = React.useState("");
-  // useEffect(() => {
-  //   if (!R.isEmpty(user)) {
-  //     dispatch(push("/home"));
-  //   }
-  // }, []);
-
-  // const handleAddTodo = () => {
-  //   request
-  //     .get("/api/example/")
-  //     .send()
-  //     .then((result) => {
-  //       console.log(result.body.message);
-  //       setResponse(result.body.message);
-  //     })
-  //     .catch();
-  // };
-
   return (
     <div className="welcome-page page">
       <div className="top">
