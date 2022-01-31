539f7acc3d29d0d7ebf8b6fc154c8bfba4f93d88
noa katz <noakatzdev@gmail.com>
2022-01-31 14:00:29 +0200

    added layout component

---
+++ client/components/organisms/PageLayout/PageLayout.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import treesIcon from '../../../assets/icons/trees.svg';
+import menuIcon from '../../../assets/icons/icon_menu.svg';
+
+import Container from 'react-bulma-companion/lib/Container';
+
+export default function PageLayout(props) {
+
+    const openMenu = () => {
+        console.log("menu should open");
+    }
+    return (
+        <div>
+            <Container className="layoutBG">
+                <img src={menuIcon} id='menuIcon' onClick={openMenu} />
+                <img src={treesIcon} id='treesIcon' />
+
+                <p id='pageTitle'>
+                    {props.pageTitle}
+                </p>
+                <main>{props.children}</main>
+            </Container>
+        </div>
+
+    );
+
+}
