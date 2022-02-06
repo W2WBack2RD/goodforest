bc54008cb90011a3ddb89b4c40b863b87d17dc5d
noa katz <noakatzdev@gmail.com>
2022-02-06 12:08:37 +0200

    added layout component fixed page wrapping and fixed componenet wrapping on all pages except the welcome page

--- client/components/environment/Main/Main.js
+++ client/components/environment/Main/Main.js
@@ -1,25 +1,23 @@
-import React, { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
-import { Switch, Route } from 'react-router';
-import ReactNotification from 'react-notifications-component';
-import { useDispatch } from 'react-redux';
-import R from 'ramda';
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import { Switch, Route } from "react-router";
+import ReactNotification from "react-notifications-component";
+import { useDispatch } from "react-redux";
+import R from "ramda";
 
-import { attemptGetUser } from '_thunks/user';
+import { attemptGetUser } from "_thunks/user";
 
-import WelcomePage from '_pages/WelcomePage';
-import LoginPage from '_pages/LoginPage';
-import RegisterPage from '_pages/RegisterPage';
-import HomePage from '_pages/HomePage';
-import TodoPage from '_pages/TodoPage';
-import SettingsPage from '_pages/SettingsPage';
-import LostPage from '_pages/LostPage';
+import WelcomePage from "_pages/WelcomePage";
+import LoginPage from "_pages/LoginPage";
+import RegisterPage from "_pages/RegisterPage";
+import HomePage from "_pages/HomePage";
+import TodoPage from "_pages/TodoPage";
+import SettingsPage from "_pages/SettingsPage";
+import LostPage from "_pages/LostPage";
 
-import Navigation from '_organisms/Navigation';
-import Footer from '_organisms/Footer';
-import PageLayout from '../../organisms/PageLayout';
-
-//      <Navigation pathname={location.pathname} />
+import Navigation from "_organisms/Navigation";
+import Footer from "_organisms/Footer";
+import PageLayout from "../../organisms/PageLayout";
 
 export default function Main({ location }) {
   const dispatch = useDispatch();
@@ -32,33 +30,34 @@ export default function Main({ location }) {
       .then(() => subscribed && setLoading(false))
       .catch(R.identity);
 
-    return () => { subscribed = false; };
+    return () => {
+      subscribed = false;
+    };
   }, []);
 
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [location.pathname]);
 
-  return !loading && (
-    <div>
-
-      <ReactNotification />
-      <PageLayout>
-
+  return (
+    !loading && (
+      <div>
+        <ReactNotification />
         <div className="main">
           <Switch>
             <Route exact path="/" component={WelcomePage} />
-            <Route path="/login" component={LoginPage} />
-            <Route path="/register" component={RegisterPage} />
-            <Route path="/home" component={HomePage} />
-            <Route path="/todo" component={TodoPage} />
-            <Route path="/settings" component={SettingsPage} />
-            <Route path="*" component={LostPage} />
+            <PageLayout>
+              <Route path="/login" component={LoginPage} />
+              <Route path="/register" component={RegisterPage} />
+              <Route path="/home" component={HomePage} />
+              <Route path="/todo" component={TodoPage} />
+              <Route path="/settings" component={SettingsPage} />
+              <Route path="*" component={LostPage} />
+            </PageLayout>
           </Switch>
         </div>
-      </PageLayout>
-    </div>
-
+      </div>
+    )
   );
 }
 
--- client/components/pages/WelcomePage/WelcomePage.js
+++ client/components/pages/WelcomePage/WelcomePage.js
@@ -1,37 +1,30 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { push } from 'connected-react-router';
-import R from 'ramda';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { push } from "connected-react-router";
+import R from "ramda";
 
-import Section from 'react-bulma-companion/lib/Section';
-import Container from 'react-bulma-companion/lib/Container';
-import Title from 'react-bulma-companion/lib/Title';
-import PageLayout from '../../organisms/PageLayout/PageLayout';
+import Section from "react-bulma-companion/lib/Section";
+import Container from "react-bulma-companion/lib/Container";
+import Title from "react-bulma-companion/lib/Title";
+import PageLayout from "../../organisms/PageLayout/PageLayout";
 
 export default function WelcomePage() {
   const dispatch = useDispatch();
-  const { user } = useSelector(R.pick(['user']));
+  const { user } = useSelector(R.pick(["user"]));
 
   useEffect(() => {
     if (!R.isEmpty(user)) {
-      dispatch(push('/home'));
+      dispatch(push("/home"));
     }
   }, []);
 
   return (
-
-
     <div className="welcome-page page">
-
       <Section>
         <Container>
-          <Title size="1">
-            Welcome Page!
-          </Title>
+          <Title size="1">Welcome Page!</Title>
         </Container>
       </Section>
-
     </div>
-
   );
 }
