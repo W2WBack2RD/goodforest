24f65536dc88018bbc5acd5ab5980b819b7c333a
Hodaya-Hanya <97478079+Hodaya-Hanya@users.noreply.github.com>
2022-02-10 18:37:15 +0200

    Login (#11)
    * Hodaya's first feature
    
    * Finish develop login page
    
    * Fix Login page after review
    
    * Fix Login page after review

--- server/routes/todos.js
+++ server/routes/todos.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { requireAuth } = require('./middleware');
 const { Todo } = require('../database/schemas');
 
-const router   = express.Router();
+const router = express.Router();
 
 module.exports = router;
 
