cc82dc22d0703f6e2db22758f308b1d2c5953762
Shoval Ichia <shovi743@gmail.com>
2022-02-22 16:08:40 +0200

    user schema

--- server/database/schemas/User.js
+++ server/database/schemas/User.js
@@ -1,28 +1,46 @@
-const mongoose = require('mongoose');
-const { MongooseAutoIncrementID } = require('mongoose-auto-increment-reworked');
-const bcrypt = require('bcryptjs');
-const R = require('ramda');
+const mongoose = require("mongoose");
+const { MongooseAutoIncrementID } = require("mongoose-auto-increment-reworked");
+const bcrypt = require("bcryptjs");
+const R = require("ramda");
 
 const { Schema } = mongoose;
 
-const userSchema = new Schema({
-  username: { type: String, lowercase: true, required: true, unique: true, immutable: true },
-  username_case: { type: String, required: true },
-  password: { type: String, required: true },
-  profile_pic: { type: String },
-  first_name: { type: String, maxlength: 20 },
-  last_name: { type: String, maxlength: 20 },
-  bio: { type: String, maxlength: 240 },
-  created_at: { type: Date, default: Date.now, immutable: true },
-  updated_at: { type: Date },
-}, { versionKey: false });
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      lowercase: true,
+      required: true,
+      unique: true,
+      immutable: true,
+    },
+    username_case: { type: String, required: true },
 
-if (process.env.NODE_ENV !== 'test') {
-  MongooseAutoIncrementID.initialise('counters');
+    city: { type: String, required: true },
+    planting_ground: { type: String, required: true },
+    full_name_responsible_personal: { type: String, required: true },
+    phone_number_responsible_person: { type: String, required: true },
+    full_name_responsible_organization: { type: String, required: true },
+    phone_number_responsible_organization: { type: String, required: true },
+    profile_pic: { type: String },
+    first_name: { type: String, maxlength: 20 },
+    last_name: { type: String, maxlength: 20 },
+    bio: { type: String, maxlength: 240 },
+    created_at: { type: Date, default: Date.now, immutable: true },
+    updated_at: { type: Date },
+    password: { type: String, required: true },
+    created_at: { type: Date, default: Date.now, immutable: true },
+    updated_at: { type: Date },
+  },
+  { versionKey: false }
+);
+
+if (process.env.NODE_ENV !== "test") {
+  MongooseAutoIncrementID.initialise("counters");
 
   userSchema.plugin(MongooseAutoIncrementID.plugin, {
-    modelName: 'User',
-    field: 'user',
+    modelName: "User",
+    field: "user",
     incrementBy: 1,
     startAt: 1,
     unique: true,
@@ -31,7 +49,7 @@ if (process.env.NODE_ENV !== 'test') {
   });
 }
 
-userSchema.virtual('full_name').get(function() {
+userSchema.virtual("full_name").get(function () {
   if (this.first_name && this.last_name) {
     return `${this.first_name} ${this.last_name}`;
   }
@@ -44,20 +62,28 @@ userSchema.virtual('full_name').get(function() {
   return undefined;
 });
 
-userSchema.virtual('initials').get(function() {
-  return this.first_name && this.last_name && `${this.first_name[0].concat(this.last_name[0]).toUpperCase()}`;
+userSchema.virtual("initials").get(function () {
+  return (
+    this.first_name &&
+    this.last_name &&
+    `${this.first_name[0].concat(this.last_name[0]).toUpperCase()}`
+  );
 });
 
-userSchema.methods.validPassword = function(password) {
+userSchema.methods.validPassword = function (password) {
   return bcrypt.compareSync(password, this.password);
 };
 
-userSchema.methods.hashPassword = function() {
+userSchema.methods.hashPassword = function () {
   return new Promise((resolve, reject) => {
     bcrypt.genSalt(10, (err1, salt) => {
-      if (err1) { reject(err1); }
+      if (err1) {
+        reject(err1);
+      }
       bcrypt.hash(this.password, salt, (err2, hash) => {
-        if (err2) { reject(err2); }
+        if (err2) {
+          reject(err2);
+        }
         this.password = hash;
         resolve(hash);
       });
@@ -65,10 +91,10 @@ userSchema.methods.hashPassword = function() {
   });
 };
 
-userSchema.methods.hidePassword = function() {
-  return R.omit(['password', '_id'], this.toObject({ virtuals: true }));
+userSchema.methods.hidePassword = function () {
+  return R.omit(["password", "_id"], this.toObject({ virtuals: true }));
 };
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
