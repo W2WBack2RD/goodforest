fd97defc7be4fbe5fae0aba85931ef00e810e0cc
Omer Shlifka <omershlifka@gmail.com>
2022-04-02 12:08:50 +0300

    try to fix session login in prod

--- server/passport/passport-config.js
+++ server/passport/passport-config.js
@@ -1,34 +1,55 @@
-const passport    = require('passport');
-const session     = require('express-session');
-const MongoStore  = require('connect-mongo');
-const uuid        = require('uuid');
-const mongoose    = require('mongoose');
+const passport = require('passport');
+const session = require('express-session');
+// const MongoStore = require('connect-mongo');
+const uuid = require('uuid');
+const mongoose = require('mongoose');
 
-const Strategies  = require('./strategies');
-const { User }    = require('../database/schemas');
+const Strategies = require('./strategies');
+const { User } = require('../database/schemas');
+
+const MongoStore = require("connect-mongo")(session);
+
 
 module.exports = app => {
+  console.log('preparing a session')
+  // const sessionConfig = {
+  //   store: MongoStore.create({
+  //     client: mongoose.connection.getClient(),
+  //     collectionName: 'sessions',
+  //   }),
+  //   genid: () => uuid.v4(),
+  //   secret: process.env.SESSION_SECRET,
+  //   resave: false,
+  //   saveUninitialized: false,
+  // };
   const sessionConfig = {
-    store: MongoStore.create({
-      client: mongoose.connection.getClient(),
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
       collectionName: 'sessions',
+      ttl: 365 * 24 * 60 * 60, // = 365 days.
     }),
-    genid: () => uuid.v4(),
     secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false,
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+      secure: app.get("env") === "production",
+    },
   };
+  console.log('initializing passport')
 
   app.use(session(sessionConfig));
   app.use(passport.initialize());
   app.use(passport.session());
 
+  console.log('serializeUser')
   passport.serializeUser((user, done) => done(null, user.id));
 
+  console.log('deserializeUser')
   passport.deserializeUser((id, done) =>
     User.findById({ _id: id })
       .then(user => done(null, user))
       .catch(err => console.warn(`err at deserialize: ${err}`)));
 
+  console.log('done')
   passport.use(Strategies.local);
 };
