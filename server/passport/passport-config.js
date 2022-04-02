f72c2280c3d7616f7d5445002ed973788915c129
Omer Shlifka <omershlifka@gmail.com>
2022-04-02 12:21:56 +0300

    try to fix session login in prod

--- server/passport/passport-config.js
+++ server/passport/passport-config.js
@@ -21,7 +21,7 @@ module.exports = app => {
   //   saveUninitialized: false,
   // };
   const sessionConfig = {
-    store: new MongoStore({
+    store: new MongoStore.create({
       mongooseConnection: mongoose.connection,
       collectionName: 'sessions',
       ttl: 365 * 24 * 60 * 60, // = 365 days.
